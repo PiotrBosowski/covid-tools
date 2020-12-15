4b83a7a70e78f25a9a155f3a188aecd7ab645460
piotrbosowski <piotr.bosowski@gmail.com>
2020-12-15 01:20:42 +0100

    Refactor dataset and datapool building

--- modules/build_datapool.py
+++ modules/build_datapool.py
@@ -3,25 +3,27 @@ import csv
 import shutil
 
 
-def build_data(input_path, output_path):
+def build_datapool(input_path, output_path):
     """
     Checks all directories in input_path directory in search of inner
     'data' directories - these are supposed to contain label-named
-    directories. Script builds merged 'data' directory in the
-    output_path location and creates a .csv file that allows for
-    backtracking the origin of particular picture and prevents from
-    naming collision.
+    directories. Script builds merged 'datapool' directory in the
+    output_path location and creates a origin.csv file that allows
+    backtracking the origin of particular picture and prevents naming
+    collision.
     """
     datasets = [os.path.join(input_path, directory)
                 for directory in os.listdir(input_path)]
     buildable_datasets = [dataset for dataset in datasets
                           if os.path.exists(os.path.join(dataset, 'data'))]
+    os.makedirs(output_path, exist_ok=False)
     with open(os.path.join(output_path, 'origins.csv'), 'x') as csv_file:
         # origin_path and new_path are relative - they relate to the
         # root folder: accordingly input_path and output_path
-        csv_writer = csv.DictWriter(csv_file, ['new_path',
-                                               'origin_path',
-                                               'origin_dataset',
+        csv_writer = csv.DictWriter(csv_file, ['img_name',
+                                               'in_datapool_path',
+                                               'in_datasource_path',
+                                               'origin_datasource',
                                                'label'])
         csv_writer.writeheader()
         for dataset in buildable_datasets:
@@ -49,13 +51,14 @@ def build_data(input_path, output_path):
                                 shutil.copy(input_file_path, output_file_path)
                                 break
                             counter += 1
-                    csv_writer.writerow({'new_path':
+                    csv_writer.writerow({'img_name': file,
+                                         'in_datapool_path':
                                         os.path.relpath(output_file_path,
                                                         output_path),
-                                         'origin_path':
+                                         'in_datasource_path':
                                         os.path.relpath(input_file_path,
                                                         input_path),
-                                         'origin_dataset':
+                                         'origin_datasource':
                                         os.path.basename(dataset),
                                          'label': label})
                     items += 1
@@ -63,4 +66,5 @@ def build_data(input_path, output_path):
 
 
 if __name__ == '__main__':
-    build_data('.', '/home/peter/media/c/Desktop/datasource')
+    build_datapool('/home/peter/media/data/covid-19/cr',
+                   '/home/peter/media/c/Desktop/datapool')
---
+++ modules/build_dataset.py
@@ -0,0 +1,83 @@
+import os
+import shutil
+import random
+import csv
+
+
+def build(args):
+    """
+    Splitting into train, test, valid wrapper.
+    """
+    build_dataset(args.path, args.output, int(args.train),
+                  int(args.valid), int(args.test),
+                  True if args.allow_imbalanced_remainings else False,
+                  args.labels)
+
+
+def curr_num_samples(images, label, num_samples, allow_imbalanced_remainings, remainings_cap):
+    if num_samples > 0:
+        return num_samples
+    elif allow_imbalanced_remainings:  # num_samples < 0 => remainings
+        return min(len(images[label]), remainings_cap)
+    else:  # not allow imbalanced remainings
+        # find lowest quantity set and take that much
+        smallest_label = len(images[min(images, key=lambda x: len(images[x]))])
+        return min(remainings_cap, smallest_label)
+
+
+def create_set(datapool_path, dataset_path, subset_name, num_samples, labels,
+               images, allow_imbalanced_remainings, remainings_cap):
+    subset_path = os.path.join(dataset_path, subset_name)
+    os.makedirs(subset_path)
+    with open(os.path.join(subset_path, 'origin.csv'), 'x') as csv_file:
+        keys = ['img_name', "in_dataset_path", 'in_datapool_path', 'in_datasource_path', 'origin_datasource', 'label']
+        csv_writer = csv.DictWriter(csv_file, keys)
+        csv_writer.writeheader()
+        for label in labels:
+            output_label_folder = os.path.join(subset_path, label)
+            os.makedirs(output_label_folder)
+            current_num = curr_num_samples(images, label, num_samples, allow_imbalanced_remainings, remainings_cap)
+            samples = random.sample(images[label], current_num)
+            for sample in samples:
+                in_dataset_path = os.path.join(output_label_folder, sample['img_name'])
+                shutil.copy(os.path.join(datapool_path, sample['in_datapool_path']),
+                            in_dataset_path)
+                images[label].remove(sample)
+                sample['in_dataset_path'] = os.path.relpath(in_dataset_path,
+                                                            dataset_path)
+                csv_writer.writerow(sample)
+
+
+def build_dataset(datapool_path, output_path, train_size, valid_size,
+                  test_size, allow_imbalanced_remainings, remainings_cap,
+                  labels):
+    os.makedirs(output_path, exist_ok=False)
+    sizes = {'train': train_size, 'valid': valid_size, 'test': test_size}
+    negative_sizes = {k: v for (k, v) in sizes.items() if v < 0}
+    positive_sizes = {k: v for (k, v) in sizes.items() if v >= 0}
+    if len(negative_sizes) > 1:
+        raise Exception("cannot create more than 1 dataset with 'include all "
+                        "remainings images' option")
+    with open(os.path.join(datapool_path, 'origins.csv'), 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        images = [row for row in csv_reader]
+    labelled_images = {}
+    for label in labels:
+        labelled_images[label] = [image for image in images
+                                  if image['label'] == label]
+        print(f'Found {len(labelled_images[label])} images of class: {label}')
+    for (name, size) in positive_sizes.items():
+        create_set(datapool_path, output_path, name, size, labels, labelled_images,
+                   allow_imbalanced_remainings, remainings_cap)
+    for (name, size) in negative_sizes.items():
+        create_set(datapool_path, output_path, name, size, labels, labelled_images,
+                   allow_imbalanced_remainings, remainings_cap)
+
+
+if __name__ == '__main__':
+    build_dataset('/home/peter/media/c/Desktop/datapool',
+                  '/home/peter/covid/datasets/0.1k-0.1k-1k',
+                  100, 100, -1,
+                  allow_imbalanced_remainings=False,
+                  remainings_cap=1000,
+                  labels=['covid', 'other', 'healthy'])
